aba5d3c17c5c90a1189cf8fdc3ad1ea634ca52df
shytkaaaa <42266824+shytkaaaa@users.noreply.github.com>
2018-08-27 14:56:04 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,8 +3,8 @@
 $recepient = "shutka92_92@mail.ru";
 $sitename = "УЧЕБНЫЙ АДАПТИВНЫЙ САЙТ №1";
 
-$name = trim($_GET["name"]);
-$phone = trim($_GET["phone"]);
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 $message = "Имя: $name \nТелефон: $phone";
