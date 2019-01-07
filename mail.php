fcac23ca8c56274cb479ee5e49d3cedd76a24380
shytkaaaa <shutka92_92@mail.ru>
2019-01-07 18:22:49 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,5 +7,5 @@ $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+$message = "Имя: $name \r\nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\r\n From: $recepient");
